e63193906cdde96f7a7ea687a2c07404285f7829
Souvik8335 <souvikghosh8335@gmail.com>
2025-08-30 13:55:43 +0530

    Milestone3

---
+++ Souvik Ghosh-Milestone 3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Souvik_Ghosh_Milestone_3___Wipro_NGA_.Net_Fullstack_Angular.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
